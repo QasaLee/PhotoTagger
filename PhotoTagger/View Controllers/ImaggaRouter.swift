3cbcc687f9677e3da725ee3b9e68033a85b56b74
NateLee <intoqasa@gmail.com>
2018-09-23 20:49:35 +0800

    URLRequestConvertible implementations are completed!

--- PhotoTagger/View Controllers/ImaggaRouter.swift
+++ PhotoTagger/View Controllers/ImaggaRouter.swift
@@ -2,18 +2,18 @@ import Alamofire
 
 public enum ImaggaRouter: URLRequestConvertible {
   
-  // 1
+  // 1 Declare constants to hold the Imagga base URL and your Basic xxx with your actual authorization header.
   enum Constants {
     static let baseURLPath = "http://api.imagga.com/v1"
-    static let authenticationToken = "Basic xxx"
+    static let authenticationToken = "Basic YWNjXzUwMThkMmI5YjUyNmQ2Zjo3ODVjZjg2YWMxYmIwOTdjYjAyZGI3YTEzZjRlM2YwZg=="
   }
   
-  // 2
+  // 2 Declare the enum cases. Each case corresponds to an api endpoint.
   case content
   case tags(String)
   case colors(String)
   
-  // 3
+  // 3 Return the HTTP method for each api endpoint.
   var method: HTTPMethod {
     switch self {
     case .content:
@@ -23,7 +23,7 @@ public enum ImaggaRouter: URLRequestConvertible {
     }
   }
   
-  // 4
+  // 4 Return the path for each api endpoint.
   var path: String {
     switch self {
     case .content:
@@ -35,7 +35,7 @@ public enum ImaggaRouter: URLRequestConvertible {
     }
   }
   
-  // 5
+  // 5 Return the parameters for each api endpoint.
   var parameters: [String: Any] {
     switch self {
     case .tags(let contentID):
@@ -47,14 +47,14 @@ public enum ImaggaRouter: URLRequestConvertible {
     }
   }
   
-  // 6
+  // 6 Use all of the above components to create a URLRequest for the requested endpoint.
   public func asURLRequest() throws -> URLRequest {
     let url = try Constants.baseURLPath.asURL()
     
     var request = URLRequest(url: url.appendingPathComponent(path))
     request.httpMethod = method.rawValue
     request.setValue(Constants.authenticationToken, forHTTPHeaderField: "Authorization")
-    request.timeoutInterval = TimeInterval(10 * 1000)
+    request.timeoutInterval = TimeInterval(10 * 1000) // 10 seconds.
     
     return try URLEncoding.default.encode(request, with: parameters)
   }
