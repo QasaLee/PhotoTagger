ab5b71e7062f39c0ecc24429ac778f5c13c0e1e4
NateLee <intoqasa@gmail.com>
2018-09-23 19:37:58 +0800

    Initial Commit

---
+++ PhotoTagger/View Controllers/ViewController.swift
@@ -0,0 +1,121 @@
+/// Copyright (c) 2018 Razeware LLC
+///
+/// Permission is hereby granted, free of charge, to any person obtaining a copy
+/// of this software and associated documentation files (the "Software"), to deal
+/// in the Software without restriction, including without limitation the rights
+/// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+/// copies of the Software, and to permit persons to whom the Software is
+/// furnished to do so, subject to the following conditions:
+///
+/// The above copyright notice and this permission notice shall be included in
+/// all copies or substantial portions of the Software.
+///
+/// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
+/// distribute, sublicense, create a derivative work, and/or sell copies of the
+/// Software in any work that is designed, intended, or marketed for pedagogical or
+/// instructional purposes related to programming, coding, application development,
+/// or information technology.  Permission for such use, copying, modification,
+/// merger, publication, distribution, sublicensing, creation of derivative works,
+/// or sale is expressly withheld.
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+/// THE SOFTWARE.
+
+import UIKit
+import SwiftyJSON
+import Alamofire
+
+class ViewController: UIViewController {
+
+  // MARK: - IBOutlets
+  @IBOutlet var takePictureButton: UIButton!
+  @IBOutlet var imageView: UIImageView!
+  @IBOutlet var progressView: UIProgressView!
+  @IBOutlet var activityIndicatorView: UIActivityIndicatorView!
+
+  // MARK: - Properties
+  private var tags: [String]?
+  private var colors: [PhotoColor]?
+
+  // MARK: - View Life Cycle
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    if !UIImagePickerController.isSourceTypeAvailable(.camera) {
+      takePictureButton.setTitle("Select Photo", for: .normal)
+    }
+  }
+
+  override func viewDidDisappear(_ animated: Bool) {
+    super.viewDidDisappear(animated)
+
+    imageView.image = nil
+  }
+
+  // MARK: - Navigation
+  override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+
+    if segue.identifier == "ShowResults",
+      let controller = segue.destination as? TagsColorsViewController {
+      controller.tags = tags
+      controller.colors = colors
+    }
+  }
+
+  // MARK: - IBActions
+  @IBAction func takePicture(_ sender: UIButton) {
+    let picker = UIImagePickerController()
+    picker.delegate = self
+    picker.allowsEditing = false
+
+    if UIImagePickerController.isSourceTypeAvailable(.camera) {
+      picker.sourceType = .camera
+    } else {
+      picker.sourceType = .photoLibrary
+      picker.modalPresentationStyle = .fullScreen
+    }
+
+    present(picker, animated: true)
+  }
+}
+
+// MARK: - UIImagePickerControllerDelegate
+extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+  func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
+// Local variable inserted by Swift 4.2 migrator.
+let info = convertFromUIImagePickerControllerInfoKeyDictionary(info)
+
+    guard let image = info[convertFromUIImagePickerControllerInfoKey(UIImagePickerController.InfoKey.originalImage)] as? UIImage else {
+      print("Info did not have the required UIImage for the Original Image")
+      dismiss(animated: true)
+      return
+    }
+
+    imageView.image = image
+
+    dismiss(animated: true)
+  }
+}
+
+extension ViewController {
+  func upload(image: UIImage,
+              progressCompletion: @escaping (_ percent: Float) -> Void,
+              completion: @escaping (_ tags: [String]?, _ colors: [PhotoColor]?) -> Void) {
+  }
+}
+
+// Helper function inserted by Swift 4.2 migrator.
+fileprivate func convertFromUIImagePickerControllerInfoKeyDictionary(_ input: [UIImagePickerController.InfoKey: Any]) -> [String: Any] {
+	return Dictionary(uniqueKeysWithValues: input.map {key, value in (key.rawValue, value)})
+}
+
+// Helper function inserted by Swift 4.2 migrator.
+fileprivate func convertFromUIImagePickerControllerInfoKey(_ input: UIImagePickerController.InfoKey) -> String {
+	return input.rawValue
+}
