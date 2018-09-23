6272952069c6cc76097f8ce22aebf66e57f2bfd0
NateLee <intoqasa@gmail.com>
2018-09-23 20:46:36 +0800

    ImaggaRouter: URLRequestConvertible

--- PhotoTagger/View Controllers/ImaggaRouter.swift
+++ PhotoTagger/View Controllers/ImaggaRouter.swift
@@ -1,2 +1,62 @@
-import Foundation
+import Alamofire
 
+public enum ImaggaRouter: URLRequestConvertible {
+  
+  // 1
+  enum Constants {
+    static let baseURLPath = "http://api.imagga.com/v1"
+    static let authenticationToken = "Basic xxx"
+  }
+  
+  // 2
+  case content
+  case tags(String)
+  case colors(String)
+  
+  // 3
+  var method: HTTPMethod {
+    switch self {
+    case .content:
+      return .post
+    case .tags, .colors:
+      return .get
+    }
+  }
+  
+  // 4
+  var path: String {
+    switch self {
+    case .content:
+      return "/content"
+    case .tags:
+      return "/tagging"
+    case .colors:
+      return "/colors"
+    }
+  }
+  
+  // 5
+  var parameters: [String: Any] {
+    switch self {
+    case .tags(let contentID):
+      return ["content": contentID]
+    case .colors(let contentID):
+      return ["content": contentID, "extract_object_colors": 0]
+    default:
+      return [:]
+    }
+  }
+  
+  // 6
+  public func asURLRequest() throws -> URLRequest {
+    let url = try Constants.baseURLPath.asURL()
+    
+    var request = URLRequest(url: url.appendingPathComponent(path))
+    request.httpMethod = method.rawValue
+    request.setValue(Constants.authenticationToken, forHTTPHeaderField: "Authorization")
+    request.timeoutInterval = TimeInterval(10 * 1000)
+    
+    return try URLEncoding.default.encode(request, with: parameters)
+  }
+
+}
